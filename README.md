0afb8bf143ec807d964c4adb441f7a923620c5c4
Alongkot <104056003+loko7@users.noreply.github.com>
2024-07-29 22:12:29 +0700

    up date gcc 29/27/2567

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Data_Structer
+
+run only in test.c
+to run run onpi to up load and compile
+crtl A
--- lap2/extra_3.c
+++ lap2/extra_3.c
@@ -3,36 +3,41 @@
 #include <stdlib.h>
 #include <math.h>
 // Define the maxdiff2 function to avoid compilation errors
-void maxdiff2(int x[], int n) {
+void maxdiff2(int x[], int n)
+{
     int i, j, diff, max = 0;
-    for (i = 0; i < n - 1; i++) {
-        for (j = i + 1; j < n; j++) {
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = i + 1; j < n; j++)
+        {
             diff = abs(x[i] - x[j]);
-            for(int l = 0; l < pow(n,2) ;l++)
-            if (diff > max) {
-                max = diff;
-            }
+            for (int l = 0; l < pow(n, 2); l++)
+                if (diff > max)
+                {
+                    max = diff;
+                }
         }
     }
 }
 
-int main () {
-   int n = 3000;
-   int* x = (int*)malloc(n * sizeof(int));
+int main()
+{
+    int n = 3000;
+    int *x = (int *)malloc(n * sizeof(int));
 
-   for(;n < 250000000; n= n*2 ){
-        //»ÃÑº¢¹Ò´
-        x = (int*)realloc(x, n * sizeof(int));
+    for (; n < 250000000; n = n * 2)
+    {
+        // ï¿½ï¿½Ñºï¿½ï¿½Ò´
+        x = (int *)realloc(x, n * sizeof(int));
         clock_t start_t, end_t;
         double total_t;
         start_t = clock();
-        //cal maxdiff2
-        maxdiff2(x,n);
+        // cal maxdiff2
+        maxdiff2(x, n);
         end_t = clock();
-        total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC; //clock
-        printf("n=%d\n Approx milliseconds: %f\n",n, total_t*1000  );
-
-   }
-   free(x);
-   return(0);
+        total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC; // clock
+        printf("n=%d\n Approx milliseconds: %f\n", n, total_t * 1000);
+    }
+    free(x);
+    return (0);
 }
