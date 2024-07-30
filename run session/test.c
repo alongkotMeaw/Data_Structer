56007538b23b4046e0d7eb75869aa79a1749de04
Alongkot <104056003+loko7@users.noreply.github.com>
2024-07-31 01:21:35 +0700

    up date poly2

--- run session/test.c
+++ run session/test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 // Declare structure of Linked List
 struct polynode
 {
@@ -26,7 +27,7 @@ void displayPoly(struct polynode *head)
             printf("%dx", ptr->coef);
         else
             printf("%dX^%d", ptr->coef, ptr->exp);
-        if (ptr->next != NULL && ptr != head)
+        if (ptr->next != NULL)
             printf("+");
         ptr = ptr->next;
     }
@@ -57,60 +58,93 @@ struct polynode *createNode(int e, int c)
     return new_node;
 }
 
+void bubbleSort(int arr[], int n)
+{
+    int temp;
+    int swapped;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        swapped = 0;
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = 1;
+            }
+        }
+
+        if (swapped == 0)
+            break;
+    }
+}
+
 int main()
 {
-    int size_a1 = 1, size_a2 = 1;
+    srand(time(NULL));
+    int i = 0;
+    int size_a1 = 0, size_a2 = 0, input;
     struct polynode *p1 = NULL, *p2 = NULL, *p3 = NULL;
     struct polynode *new_node;
     // 1st polynomial
     int *a1 = (int *)malloc(size_a1 * sizeof(int));
     printf("==== Create 1st Polynomial ====\n");
-    do
+    for (;;)
     {
         printf("Enter exponential 1:");
-        scanf("%d", &a1[size_a1]);
-        printf("a1 == %d", a1[size_a1]);
+        scanf("%d", &input);
+        if (input < 0)
+            break;
         size_a1++;
         a1 = (int *)realloc(a1, size_a1 * sizeof(int));
-    } while (a1[size_a1] > -1);
+        a1[size_a1 - 1] = input;
+    }
 
-    // 2st polynomial
-    int *a2 = (int *)malloc(size_a2 * sizeof(int));
-    printf("==== Create 2nd Polynomial ====\n");
-    do
-    {
-        printf("Enter exponential 2:");
-        scanf("%d", &a2[size_a1 - 1]);
-        size_a2++;
-        a2 = (int *)realloc(a2, size_a2 * sizeof(int));
-    } while (a2[size_a1 - 1] > -1);
+    // sort exponential
 
-    int i, n = 5;
-
-    // Create linked list for representing polynomial
-    // Loop for create 1st polynomial (add node at beginning)
-    // int n = sizeof(a1) / sizeof(a1[0]); // Number of coefficients
-    i = 0;
-    struct polynode *head1 = createNode(n - 1, a1[n - 1]);
+    bubbleSort(a1, size_a1);
+    for (int i = 0; i < size_a1; i++)
+        printf("%d ", a1[i]);
+    printf("size a1 = %d", size_a1);
+    // asian in linklist
+    struct polynode *head1 = createNode(a1[size_a1 - 1], (rand() % 30 + 1));
     struct polynode *current = head1;
 
-    for (int i = 1; i < n; i++)
+    for (int i = 1; i < size_a1; i++)
     {
-        struct polynode *new_node = createNode(n - 1 - i, a1[n - i - 1]);
+        struct polynode *new_node = createNode(a1[size_a1 - i - 1], (rand() % 30 + 1));
         current->next = new_node;
         current = new_node;
     }
     printf("1st Polynomial: ");
     displayPoly(head1);
 
-    // Loop for create 2nd polynomial (add node at beginning)
-    i = 0;
-    struct polynode *head2 = createNode(n - 1, a2[n - 1]);
+    // 2st polynomial
+    int *a2 = (int *)malloc(size_a2 * sizeof(int));
+    printf("==== Create 2nd Polynomial ====\n");
+    for (;;)
+    {
+        printf("Enter exponential 2:");
+        scanf("%d", &input);
+        if (input < 0)
+            break;
+        size_a2++;
+        a2 = (int *)realloc(a2, size_a1 * sizeof(int));
+        a2[size_a2 - 1] = input;
+    }
+
+    // sort exponential
+    bubbleSort(a2, size_a2);
+    // asian in linklist
+    struct polynode *head2 = createNode(a2[size_a2 - 1], (rand() % 30 + 1));
     struct polynode *current2 = head2;
 
-    for (int i = 1; i < n; i++)
+    for (int i = 1; i < size_ar2; i++)
     {
-        struct polynode *new_node = createNode(n - 1 - i, a2[n - i - 1]);
+        struct polynode *new_node = createNode(a2[size_a2 - i - 1], (rand() % 30 + 1));
         current2->next = new_node;
         current2 = new_node;
     }
