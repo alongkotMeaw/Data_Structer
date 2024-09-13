7d2f1ed8b57ac571968fb625be163c58ebcff78f
Alongkot <Alongkotmeam16@gmail.com>
2024-09-13 12:18:25 +0700

    test_run_muti_file

---
+++ learh_create_Header_file/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "myhead.c"
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    printf("sum = %d", sum(7, 5));
+    return 0;
+}
---
+++ learh_create_Header_file/myhead.h
@@ -0,0 +1,6 @@
+#ifndef MYHEAD_H
+#define MYHEAD_H
+
+int sum(int x, int y);
+
+#endif
